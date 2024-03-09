082d282a079a7c10b3c3688c961638f7c2b1516d
Arturo <hernandez020610@gmail.com>
2024-03-09 16:56:48 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,15 @@ export const routes: Routes = [
     { path: 'register', loadComponent:() => import('./bicicletas/register/register.component').then((c) => c.RegisterComponent) },
     { path: 'catalogos', loadComponent:() => import('./bicicletas/catalogos/catalogos.component').then((c) => c.CatalogosComponent)},
     { path: 'home', loadComponent:() => import('./bicicletas/home/home.component').then((c) => c.HomeComponent)},
+    { path: 'compras', loadComponent:() => import('./bicicletas/compras/compras.component').then((c) => c.ComprasComponent)},
+    { path: 'detallecompras', loadComponent:() => import('./bicicletas/detallecompras/detallecompras.component').then((c) => c.DetallecomprasComponent) },
+    { path: 'detallepedidos', loadComponent:() => import('./bicicletas/detallepedidos/detallepedidos.component').then((c) => c.DetallepedidosComponent) },
+    { path: 'favoritos', loadComponent:() => import('./bicicletas/favoritos/favoritos.component').then((c) => c.FavoritosComponent) },
+    { path: 'items', loadComponent:() => import('./bicicletas/items/items.component').then((c) => c.ItemsComponent) },
+    { path: 'metodopagos', loadComponent:() => import('./bicicletas/metodopagos/metodopagos.component').then((c) => c.MetodopagosComponent) },
+    { path: 'modelos', loadComponent:() => import('./bicicletas/modelos/modelos.component').then((c) => c.ModelosComponent) },
+    { path: 'pedidos', loadComponent:() => import('./bicicletas/pedidos/pedidos.component').then((c) => c.PedidosComponent) },
+    { path: 'resenas', loadComponent:() => import('./bicicletas/resenas/resenas.component').then((c) => c.ResenasComponent) },
+    { path: 'users', loadComponent:() => import('./bicicletas/users/users.component').then((c) => c.UsersComponent)},
     { path: '**', loadComponent:() => import('./bicicletas/login/login.component').then((c) => c.LoginComponent), pathMatch: 'full' }
 ];
