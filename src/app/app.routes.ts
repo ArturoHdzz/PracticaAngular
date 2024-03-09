f5ce34151e280eb0ce62be6297acec4eec7aa48d
Arturo <hernandez020610@gmail.com>
2024-03-09 15:28:08 -0600

    Algunos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', loadComponent:() => import('./bicicletas/login/login.component').then((c) => c.LoginComponent) },
+    { path: 'register', loadComponent:() => import('./bicicletas/register/register.component').then((c) => c.RegisterComponent) },
+    { path: 'catalogos', loadComponent:() => import('./bicicletas/catalogos/catalogos.component').then((c) => c.CatalogosComponent)},
+    { path: 'home', loadComponent:() => import('./bicicletas/home/home.component').then((c) => c.HomeComponent)},
+
+    { path: '**', loadComponent:() => import('./bicicletas/home/home.component').then((c) => c.HomeComponent), pathMatch: 'full' }
+];
