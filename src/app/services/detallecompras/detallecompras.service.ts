de58bc9ca3028db9ac70307b91027c67ad2d2db5
Arturo <hernandez020610@gmail.com>
2024-03-12 14:23:53 -0600

    otro crud

--- src/app/services/detallecompras/detallecompras.service.ts
+++ src/app/services/detallecompras/detallecompras.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { ApiResponse, IDetalleCompra } from '../../shared/models/DetalleCompra';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetallecomprasService {
 
-  constructor() { }
+  todos = "http://127.0.0.1:8000/api/auth/detallecompra";
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<ApiResponse<IDetalleCompra[]>>{
+    return this.http.get<ApiResponse<IDetalleCompra[]>>(`${this.todos}`)
+  }
+
+  get(id: string): Observable<ApiResponse<IDetalleCompra>>{
+    return this.http.get<ApiResponse<IDetalleCompra>>(`${this.todos}/${id}`)
+  }
+
+  create(detalleCompra: IDetalleCompra): Observable<ApiResponse<IDetalleCompra>>{
+    return this.http.post<ApiResponse<IDetalleCompra>>(`${this.todos}`, detalleCompra)
+  }
+
+  update(id: string, detalleCompra: IDetalleCompra){
+    return this.http.put<ApiResponse<IDetalleCompra>>(`${this.todos}/${id}`, detalleCompra)
+  }
+
+  delete(id: string): Observable<ApiResponse<IDetalleCompra>>{
+    return this.http.delete<ApiResponse<IDetalleCompra>>(`${this.todos}/${id}`)
+  }
 }
