ec24eaafe3f8b8ddee774e8efe7853ee407f2467
Arturo <hernandez020610@gmail.com>
2024-03-11 18:51:22 -0600

    cosas

--- src/app/bicicletas/users/users-form/user-form/user-form.component.ts
+++ src/app/bicicletas/users/users-form/user-form/user-form.component.ts
@@ -3,11 +3,12 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 import { UsersService } from '../../../../services/users.service';
 import { IUser } from '../../../../shared/models/User';
 import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
