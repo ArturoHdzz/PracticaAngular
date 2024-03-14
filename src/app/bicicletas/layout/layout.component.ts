e94544c91b9bac99a519bf09f20abf7cc65fe5a4
JorgeFullScout777 <jorgefullscout@gmail.com>
2024-03-13 18:40:36 -0600

    cambios

--- src/app/bicicletas/layout/layout.component.ts
+++ src/app/bicicletas/layout/layout.component.ts
@@ -1,13 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  roleId: any;
+  Error: string | null;
+
+  constructor(private http: HttpClient, private router: Router){
+     this.Error = null;
+     this.roleId = null;
+     this.rolUser();
+  }
+
+  rolUser(){
+    
+    this.http.get('http://127.0.0.1:8000/api/auth/roluser', ).subscribe(
+      (res: any) => {
+        this.roleId = res.role_id;
+        console.log(this.roleId)
+        this.Error = null;
+      },
+      (error) => {
+        console.log(error)
+        this.Error = "El correo o la contraseña son invalidos, porfavor, reviselas bien."
+      }
+    );
+  }
 
 }
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,18 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { TokenService } from '../services/token.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   
   const router = inject(Router)
   
   const localData = localStorage.getItem('TOKEN');
-  if(localData != null){
+  if(localData != ''){
     return true;
   }else{
     router.navigateByUrl('/login')
     return false;
   }
+
 };
