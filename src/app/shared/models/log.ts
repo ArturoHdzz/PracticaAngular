132e77bbda25b08f28a67f1d74deba4fd6c8774c
Arturo <hernandez020610@gmail.com>
2024-03-17 12:03:42 -0600

    vista logs

--- src/app/shared/models/log.ts
+++ src/app/shared/models/log.ts
@@ -3,9 +3,23 @@ export interface ApiResponse<T>{
     data: T;
   }
 
+export interface IUser{
+  id?: string;
+  name: string;
+  email: string;
+  is_active: string;
+  role_id: string;
+  created_at: string;
+  updated_at: string;
+}
+
 export interface Ilogs{
     id?: string;
-    comentario: string;
-    calificacion: string;
-    fecha: string;
+    route: string;
+    method: string;
+    values: string;
+    user_id: string;
+    updated_at: string;
+    created_at: string;
+    user: IUser;
   }
