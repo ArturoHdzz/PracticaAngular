ee590937e694c7137422381d93eee3a9f1b18e69
Arturo <hernandez020610@gmail.com>
2024-03-19 13:46:37 -0600

    polling

--- src/app/bicicletas/logs/logs.component.ts
+++ src/app/bicicletas/logs/logs.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModelComponent } from '../../shared/ui/model/model.component';
 import { CommonModule } from '@angular/common';
 import { Ilogs } from '../../shared/models/log';
 import { LogsService } from '../../services/logs/logs.service';
 import { ToastrService } from 'ngx-toastr';
 import { HttpClient } from '@angular/common/http';
+import { Subscription, interval, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-logs',
@@ -13,13 +14,42 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './logs.component.html',
   styleUrl: './logs.component.css'
 })
-export class LogsComponent implements OnInit{
+export class LogsComponent implements OnInit, OnDestroy{
   datos: Ilogs[]=[];
+  timeInterval: Subscription;
 
-  constructor(private Service: LogsService, private toastService: ToastrService, private http: HttpClient){}
+  constructor(private Service: LogsService, private toastService: ToastrService, private http: HttpClient){
+    this.timeInterval = new Subscription();
+  }
 
   ngOnInit(): void {
-    this.getAll();
+    this.startPolling();
+  }
+
+  ngOnDestroy(): void {
+    this.stopPolling();
+  }
+
+  startPolling() {
+    this.timeInterval = interval(5000)
+      .pipe(
+        switchMap(() => this.Service.getAll())
+      )
+      .subscribe({
+        next: (response) => {
+          this.datos = response.data;
+        },
+        error: (error) => {
+          console.error('Error al obtener registros:', error);
+          this.stopPolling();
+        }
+      });
+  }
+
+  stopPolling() {
+    if (this.timeInterval) {
+      this.timeInterval.unsubscribe();
+    }
   }
 
   getAll() {
